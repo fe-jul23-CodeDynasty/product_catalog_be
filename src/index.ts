161fb452a1ea2c2562958b138c933a78fb2eba9f
Denis Chakhalian <my.chahalyan@gmail.com>
2023-10-23 20:05:57 +0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import express from "express";
 
 export const server = express();
 server.get('/', (req, res) => {
-  res.send('GET request to the homepage')
+  ressdf
+ sdfsd 
+  .send('GET request to the homepage')
 })
 
 server.get('/test', (req, res) => {
