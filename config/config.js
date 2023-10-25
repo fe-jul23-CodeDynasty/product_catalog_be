084566284025ba71e029662e425e0ffaf16545c2
Denis Chakhalian <my.chahalyan@gmail.com>
2023-10-25 14:55:05 +0300

    feat: Create products table (need to write database migration)

---
+++ config/config.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const URI = process.env.DB_URL;
+
+const dbConfig = {
+  url: URI,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+};
+
+module.exports = {
+  development: { ...dbConfig },
+  test: { ...dbConfig },
+  production: { ...dbConfig }
+};
